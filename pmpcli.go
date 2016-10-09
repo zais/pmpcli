6be600f6d8d29c4032b06e243da0c36b5ec7b73c
zais <bkzais@bk.ru>
2016-10-09 09:54:10 +0300

    arguments parsing

--- pmpcli.go
+++ pmpcli.go
@@ -13,6 +13,7 @@ import (
 	"net/http/cookiejar"
 	"net/url"
 	"os"
+	"regexp"
 	"strings"
 	"unicode/utf8"
 
@@ -20,8 +21,69 @@ import (
 )
 
 // -----------------------------------------------------------------------------
+// Version
+// -----------------------------------------------------------------------------
+
+// BUILD number
+const BUILD ="201610090952"
+
+// VERSION of this piece of ***
+const VERSION = "0.1.0." + BUILD
+
+// VERSIONAPI engine/plugin versions
+const VERSIONAPI = "engine: 8.5.0.8502 / plugin: 1.0.2.4"
+
+// -----------------------------------------------------------------------------
+// Log level
+// -----------------------------------------------------------------------------
+
+// DEBUG is debug
+var DEBUG = false
+
+// VERBOSE output
+var VERBOSE = false
+
+// Info prints if verbose
+func Info(v ...interface{}) {
+	if VERBOSE {
+		log.Println(v...)
+	}
+}
+
+// Infof prints if verbose
+func Infof(format string, v ...interface{}) {
+	if VERBOSE {
+		log.Printf(format, v...)
+	}
+}
+
+// Debug prints if debug
+func Debug(v ...interface{}) {
+	if DEBUG {
+		log.Println(v...)
+	}
+}
+
+// Debugf prints if debug
+func Debugf(format string, v ...interface{}) {
+	if DEBUG {
+		log.Printf(format, v...)
+	}
+}
+
+// -----------------------------------------------------------------------------
 // PMP Client
 // -----------------------------------------------------------------------------
+
+// PMPMode defines client behavior
+type PMPMode int
+
+const (
+	// PMPModePlugin emulate plugin
+	PMPModePlugin PMPMode = 1 + iota
+	// PMPModeBrowser emulate browser
+	PMPModeBrowser
+)
 
 // PMPClient provides methods to setup session and get passwords
 type PMPClient struct {
@@ -31,7 +93,25 @@ type PMPClient struct {
 	User     string
 	Domain   string
 	Org      string
+	mode     PMPMode
 	//TODO: logger as dependency/part of struct/constructor
+}
+
+// PMPConfig holds values for PMPClient constructor
+type PMPConfig struct {
+	LoginURL   string
+	Domain     string
+	User       string
+	Password   string
+	Org        string
+	System     string
+	Account    string
+	Ticket     string
+	Reason     string
+	SetEnv     bool
+	IgnoreCert bool
+	Verbose    bool
+	Debug      bool
 }
 
 // PMPEntry holds user/password for system
@@ -47,26 +127,35 @@ func (e PMPEntry) String() (str string) {
 }
 
 // NewPMPClient creates PMPClient
-// if not secure => ignores ssl certs
-func NewPMPClient(LoginURL string, secure bool) (pc *PMPClient, err error) {
-	u, err := url.Parse(LoginURL)
+func NewPMPClient(cfg PMPConfig) (pc *PMPClient, err error) {
+	u, err := url.Parse(cfg.LoginURL)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	tc, err := NewTinyClient(secure)
+	tc, err := NewTinyClient(map[string]string{
+		"User-Agent":  "Mozilla/5.0 (Linux 6.1; rv:48.0) Gecko/20100101 Firefox/48.0",
+		"requestFrom": "pmpmobilenative",
+		"clientType":  "12",
+	}, cfg.IgnoreCert)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	return &PMPClient{tc, LoginURL, u.Scheme + "://" + u.Host, "", "", ""}, err
+	baseURL := u.Scheme + "://" + u.Host
+	return &PMPClient{
+		TinyClient: tc,
+		LoginURL:   cfg.LoginURL,
+		BaseURL:    baseURL,
+		Domain:     cfg.Domain,
+		Org:        cfg.Org,
+		mode:       PMPModePlugin,
+	}, err
 }
 
 // SetUserDomain gets domain for given user
 func (pc *PMPClient) SetUserDomain() (err error) {
 	// get userdomain
 	u := pc.BaseURL + "/login/AjaxResponse.jsp?RequestType=GetUserDomainName&userName=" + pc.User
-	if VERBOSE {
-		log.Println("Request:", u)
-	}
+	Info("Request:", u)
 	body, status, err := pc.Get(u)
 	if err != nil {
 		log.Fatalln(err)
@@ -86,6 +175,11 @@ func (pc *PMPClient) Login(user string, password string) (success bool, err erro
 	if err != nil {
 		log.Fatalln(err)
 	}
+	// TODO: loginURL can have meta redirect
+	// r := regexp.MustCompile(`(?i)<meta http-equiv="refresh" content="\d+;URL=([^"]+)">`)
+	// if m := r.FindStringSubmatch("html to parse")[1]; m != nil {
+	//   ...
+	// }
 	if status == 200 && err == nil {
 		if pc.Org, err = htmlElementValByID(body, "ORGN_NAME"); err != nil {
 			log.Fatalln(err)
@@ -107,9 +201,7 @@ func (pc *PMPClient) Login(user string, password string) (success bool, err erro
 	// submit login form
 	jsessionid := pc.GetCookie(pc.BaseURL, "JSESSIONID")
 	u := pc.BaseURL + "/j_security_check;jsessionid=" + jsessionid
-	if VERBOSE {
-		log.Println("Request:", u)
-	}
+	Info("Request:", u)
 	vals := url.Values{
 		"BROWSER_NAME":  {"FF"},
 		"ORGN_NAME":     {pc.Org},
@@ -126,9 +218,11 @@ func (pc *PMPClient) Login(user string, password string) (success bool, err erro
 	if status == 200 && err == nil {
 		success = true
 	}
-	if DEBUG {
-		log.Println("Values:", vals)
-		log.Println("[", status, "]", "Login", "pmpcc="+pc.GetCookie(pc.BaseURL, "pmpcc"))
+	Debug("Values:", vals)
+	pmpcc := pc.GetCookie(pc.BaseURL, "pmpcc")
+	Debug("[", status, "]", "Login", "pmpcc="+pmpcc)
+	if pmpcc == "" {
+		log.Fatalln(errors.New("Login failed"))
 	}
 	return
 }
@@ -139,9 +233,7 @@ func (pc *PMPClient) LogOut() (err error) {
 	pmpcc := pc.GetCookie(pc.BaseURL, "pmpcc")
 	// submit login form
 	u := pc.BaseURL + "/jsp/xmlhttp/AjaxResponse.jsp"
-	if VERBOSE {
-		log.Println("Request:", u)
-	}
+	Info("Request:", u)
 	_, status, err := pc.PostForm(u, url.Values{
 		"RequestType": {"InvalidateSession"},
 		"pmpcp":       {pmpcc},
@@ -159,9 +251,7 @@ func (pc *PMPClient) LogOut() (err error) {
 func (pc *PMPClient) GetOrgs() (orgs map[string]string, err error) {
 	// get ORGs
 	u := pc.BaseURL + "/ajaxservlet/AjaxServlet?action=searchOrganization"
-	if VERBOSE {
-		log.Println("Request:", u)
-	}
+	Info("Request:", u)
 	resp, status, err := pc.Get(u)
 	if err != nil {
 		log.Fatalln(err)
@@ -189,9 +279,7 @@ func (pc *PMPClient) GetOrgs() (orgs map[string]string, err error) {
 	} else {
 		err = errors.New("Bad status " + data.Status + " for json request")
 	}
-	if DEBUG {
-		log.Println("JSON", data)
-	}
+	Debug("JSON", data)
 	return
 }
 
@@ -212,9 +300,7 @@ func (pc *PMPClient) ChangeOrg(org string) (err error) {
 	pmpcc := pc.GetCookie(pc.BaseURL, "pmpcc")
 	// change ORG
 	u := pc.BaseURL + "/jsp/xmlhttp/OrgAjaxResponse.jsp?RequestType=organizationChange&SUBREQUEST=XMLHTTP"
-	if VERBOSE {
-		log.Println("Request:", u)
-	}
+	Info("Request:", u)
 	body, status, err := pc.PostForm(u, url.Values{
 		"ORGID": {orgID},
 		"pmpcp": {pmpcc},
@@ -222,9 +308,7 @@ func (pc *PMPClient) ChangeOrg(org string) (err error) {
 	if status != 200 {
 		err = errors.New("Failed to change org to '" + org + "'")
 	}
-	if DEBUG {
-		log.Println("[", status, "]", body)
-	}
+	Debug("[", status, "]", body)
 	return
 }
 
@@ -234,9 +318,7 @@ func (pc *PMPClient) GetPassword(entry *PMPEntry, reason string, addreason strin
 	pmpcc := pc.GetCookie(pc.BaseURL, "pmpcc")
 	// query password
 	u := pc.BaseURL + "/jsp/xmlhttp/PasswdRetriveAjaxResponse.jsp?RequestType=PasswordRetrived"
-	if VERBOSE {
-		log.Println("Request:", u)
-	}
+	Info("Request:", u)
 	resp, status, err := pc.PostForm(u, url.Values{
 		"resource":  {entry.system},
 		"account":   {entry.user},
@@ -261,31 +343,28 @@ func (pc *PMPClient) GetPassword(entry *PMPEntry, reason string, addreason strin
 // NOT thread safe
 type TinyClient struct {
 	Client    *http.Client
-	UserAgent string
+	Header    map[string]string
 	cookieJar *cookiejar.Jar
 }
 
 // NewTinyClient creates new client
-// if secure=false => ignores ssl certificates
-func NewTinyClient(secure bool) (tc *TinyClient, err error) {
-	// FireFox is default UserAgent
-	const agent = "User-Agent: Mozilla/5.0 (Linux 6.1; rv:48.0) Gecko/20100101 Firefox/48.0"
-
+// if IgnoreCert=true => ignores ssl certificates
+func NewTinyClient(header map[string]string, IgnoreCert bool) (tc *TinyClient, err error) {
 	jar, err := cookiejar.New(nil)
 	if err != nil {
 		return
 	}
 
 	// ignore untrusted certs
-	if secure {
-		client := &http.Client{Jar: jar}
-		tc = &TinyClient{client, agent, jar}
-	} else {
+	if IgnoreCert {
 		tr := &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		}
 		client := &http.Client{Transport: tr, Jar: jar}
-		tc = &TinyClient{client, agent, jar}
+		tc = &TinyClient{client, header, jar}
+	} else {
+		client := &http.Client{Jar: jar}
+		tc = &TinyClient{client, header, jar}
 	}
 	return tc, err
 }
@@ -321,14 +400,21 @@ func (c *TinyClient) DoReq(req *http.Request) (str string, status int, err error
 	return string(b), status, nil
 }
 
+// prepareReq deals with agent and extra headers
+func (c *TinyClient) prepareReq(req *http.Request) {
+	for k, v := range c.Header {
+		req.Header.Add(k, v)
+	}
+}
+
 // PostForm submits form provided as url.Values
 func (c *TinyClient) PostForm(url string, vals url.Values) (body string, status int, err error) {
 	req, err := http.NewRequest("POST", url, bytes.NewBufferString(vals.Encode()))
 	if err != nil {
 		return
 	}
+	c.prepareReq(req)
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("User-Agent", c.UserAgent)
 	return c.DoReq(req)
 }
 
@@ -338,22 +424,8 @@ func (c *TinyClient) Get(url string) (body string, status int, err error) {
 	if err != nil {
 		return
 	}
-	req.Header.Add("User-Agent", c.UserAgent)
+	c.prepareReq(req)
 	return c.DoReq(req)
-}
-
-func tinyClientUsage() {
-	client := &TinyClient{}
-	client.UserAgent = "User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:48.0) Gecko/20100101 Firefox/48.0"
-	rawURL := os.Args[1]
-
-	resp, status, err := client.PostForm(rawURL, url.Values{"custname": {"lalala"}})
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println("status:", status, "body:", resp)
-
 }
 
 // -----------------------------------------------------------------------------
@@ -393,59 +465,129 @@ func htmlElementValByID(htmlStr string, name string) (str string, err error) {
 }
 
 // -----------------------------------------------------------------------------
-// Main
+// Other helpers
 // -----------------------------------------------------------------------------
 
-// DEBUG is debug
-const DEBUG = false
+func printHelp() {
+	fmt.Println("Version:", VERSION, "( for", VERSIONAPI, ")")
+	fmt.Println("Usage  :")
+	fmt.Println("  pmpcli l=<login_url> [d=<domain>] u=<user> p=<pass> o=<org> s=<system> a=<account> t=<ticket> r=<reason> [cert=ignore] [<env>|<verbose>|<debug>]")
+	fmt.Println("Example:")
+	fmt.Println("  pmpcli l=https://127.0.0.1:7272 u=user p=pass o=org1 s=serv a=root t=inc1234 r=check")
+}
 
-// VERBOSE output
-const VERBOSE = false
-
-// BUILD number
-const BUILD ="201610081018"
-
-// VERSION of this piece of ***
-const VERSION = "0.1.0." + BUILD
-
-// VERSIONAPI engine/plugin versions
-const VERSIONAPI = "engine: 8.5.0.8502 / plugin: 1.0.2.4"
-
-func main() {
-	if len(os.Args) != 9 {
-		fmt.Println("Version:", VERSION, "( for", VERSIONAPI, ")")
-		fmt.Println("Usage  :")
-		fmt.Println("  pmpcli <login_url> <user> <pass> <org> <system> <account> <ticket> <reason>")
-		fmt.Println("Example:")
-		fmt.Println("  pmpcli https://127.0.0.1:7272 user pass org1 serv root inc1234 check")
-		return
+func parseArgs(args []string) (cfg PMPConfig) {
+	//fmt.Println(len(os.Args))
+	if len(os.Args) < 2 {
+		printHelp()
+		os.Exit(1)
 	}
-	rawurl := os.Args[1]
-	user := os.Args[2]
-	pass := os.Args[3]
-	org := os.Args[4]
-	system := os.Args[5]
-	account := os.Args[6]
-	ticket := os.Args[7]
-	reason := os.Args[8]
+	for _, a := range os.Args[1:] {
+		Debug("arg:", a)
+		s := strings.SplitN(a, "=", 2)
+		if s == nil || len(s) == 1 {
+			switch a {
+			case "env":
+				cfg.SetEnv = true
+			case "debug":
+				cfg.Debug = true
+			case "verbose":
+				cfg.Verbose = true
+			default:
+				log.Fatalln("ERROR: wrong arg", a)
+			}
+		} else {
+			k := s[0]
+			v := s[1]
+			switch k {
+			case "l":
+				Debug("login:", v)
+				cfg.LoginURL = v
+			case "d":
+				Debug("domain:", v)
+				cfg.Domain = v
+			case "u":
+				Debug("user:", v)
+				cfg.User = v
+			case "p":
+				Debug("password:", v)
+				cfg.Password = v
+			case "o":
+				Debug("org:", v)
+				cfg.Org = v
+			case "s":
+				Debug("system:", v)
+				cfg.System = v
+			case "a":
+				Debug("account:", v)
+				cfg.Account = v
+			case "t":
+				Debug("ticket:", v)
+				cfg.Ticket = v
+			case "r":
+				Debug("reason:", v)
+				cfg.Reason = v
+			case "cert":
+				Debug("certificate:", v)
+				if v == "ignore" {
+					cfg.IgnoreCert = true
+				}
+			default:
+				log.Fatalln("ERROR: Wrong option:", a)
+			}
+		}
+	}
+	return
+}
 
-	pc, err := NewPMPClient(rawurl, false)
+func getPasswords(cfg PMPConfig) {
+	pc, err := NewPMPClient(cfg)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	success, err := pc.Login(user, pass)
+	success, err := pc.Login(cfg.User, cfg.Password)
 	if err != nil || !success {
 		log.Fatalln(err)
 	}
-	if err = pc.ChangeOrg(org); err != nil {
+	if err = pc.ChangeOrg(cfg.Org); err != nil {
 		log.Fatalln(err)
 	}
-	entry := &PMPEntry{system: system, user: account}
-	if err = pc.GetPassword(entry, ticket, reason); err != nil {
+	entry := &PMPEntry{system: cfg.System, user: cfg.Account}
+	if err = pc.GetPassword(entry, cfg.Ticket, cfg.Reason); err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(entry.user + " @ " + entry.system + " / " + entry.password)
+	if cfg.SetEnv {
+		v := entry.system + "_" + entry.user
+		// sanitize variable name
+		re := regexp.MustCompile("[[:alnum:]_]+")
+		v = strings.Join(re.FindAllString(v, -1), "_")
+		// sanitize password
+		p := strings.Replace(entry.password, "'", `'"'"'`, -1)
+		// print env
+		fmt.Println("echo '# Setting " + v +
+			" for " + entry.user + " @ " + entry.system +
+			"'; " + v + "='" + p + "'")
+	} else {
+		fmt.Println(entry.user + " @ " + entry.system + " / " + entry.password)
+	}
 	if pc.LogOut(); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+// -----------------------------------------------------------------------------
+// Main
+// -----------------------------------------------------------------------------
+
+func main() {
+	log.SetFlags(0)
+	cfg := parseArgs(os.Args)
+	if cfg.Verbose {
+		VERBOSE = true
+	}
+	if cfg.Debug {
+		DEBUG = true
+	}
+	Debug("PMPConfig:", cfg)
+	getPasswords(cfg)
 }
